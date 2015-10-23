4142fd06196572feac05aa521fa3693039788838
Cody Bouscaren <codybousc@gmail.com>
2015-10-23 10:36:35 -0400

    Regex-Adventure 3

--- regex-adventure.js
+++ regex-adventure.js
@@ -1,14 +1,21 @@
 //Regex Adventure
-#1
+// #1
+//
+// module.exports = function(str) {
+//   var reger = /LITERALLY/;
+//   return reger.test(str);
+// }
+//
+// #2
+//
+// module.exports = function(str) {
+//   var reger = /^LITERALLY/;
+//   return reger.test(str);
+// }
+//
+// #3
 
 module.exports = function(str) {
-  var reger = /LITERALLY/;
-  return reger.test(str);
-}
-
-#2
-
-module.exports = function(str) {
-  var reger = /^LITERALLY/;
+  var reger = /BANANAS$/;
   return reger.test(str);
 }
